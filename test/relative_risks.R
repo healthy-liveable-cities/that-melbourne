be8f88d4425834974042a09a1df51661aab03ead
Ali Abbas <m.aliabbas@gmail.com>
2021-07-30 16:21:16 +0100

    Add script to draw and plot samples from ITHIM-R package

--- test/relative_risks.R
+++ test/relative_risks.R
@@ -178,3 +178,71 @@ rr_2
 # > rr_2
 # [1] 1.000383
 
+################## Read PA DR curves ##################
+rm(list = ls())
+require(ithimr)
+
+global_path <- file.path(find.package('ithimr',lib.loc=.libPaths()), 'extdata/global/')
+global_path <- paste0(global_path, "/")
+
+DISEASE_INVENTORY <- read.csv(paste0(global_path,"dose_response/disease_outcomes_lookup.csv"))
+
+list_of_files <- list.files(path = paste0(global_path,"dose_response/drpa/extdata/"), recursive = TRUE, pattern = "\\.csv$", full.names = TRUE)
+for (i in 1:length(list_of_files)){
+  assign(stringr::str_sub(basename(list_of_files[[i]]), end = -5),
+         readr::read_csv(list_of_files[[i]],col_types = cols()),
+         pos = 1)
+}
+
+cols <- rainbow(sum(DISEASE_INVENTORY$physical_activity == 1))
+cause_indices <- which(DISEASE_INVENTORY$physical_activity == 1)
+
+############# sample dose-response curves
+nsamples <- 100
+parameters <- ithimr::ithim_setup_parameters(NSAMPLES=nsamples,
+                                             PA_DOSE_RESPONSE_QUANTILE=T)
+
+spl_cause <- list()
+pdf('test/PA_dose_response_sample.pdf',width=11,height=11)
+par(mar=c(5,5,2,1),mfrow=c(3,3))
+for ( j in 1:length(cause_indices)){
+  cause <- as.character(DISEASE_INVENTORY$pa_acronym[cause_indices[j]])
+  
+  if (cause %in% c("all_cause", "coronary_heart_disease", "stroke", "diabetes", "total_cancer"))
+    doses_vector <- get(paste0(cause, '_mortality')) %>% dplyr::select(dose) %>% unlist()
+  else
+    doses_vector <- get(paste0(cause, '_all')) %>% dplyr::select(dose) %>% unlist()
+  
+  # get name of disease
+  # print(cause)
+  for(seed in 1:nsamples){
+    for(i in 1:length(parameters))
+      assign(names(parameters)[i],parameters[[i]][[seed]],pos=1)
+    return_vector <- ithimr::PA_dose_response(cause = cause,dose = doses_vector)
+    
+    # print(get(paste0('PA_DOSE_RESPONSE_QUANTILE_',cause)))
+    temp_df <- data.frame(dose = doses_vector, RR = return_vector)
+    names(temp_df)[2] <- paste0("RR_", seed)
+    spl_cause[[cause]][[seed]] <- temp_df
+    
+    if(seed==1){
+      plot(doses_vector,return_vector$rr,type='l',ylim=0:1,main=cause,ylab='Relative risk',xlab='Dose',
+           frame=F,col=cols[j],xlim=range(doses_vector),cex.axis=1.5,cex.lab=1.5,cex.main=1.5,lwd=1)
+    }else{
+      lines(doses_vector,return_vector$rr,col=cols[j],lwd=1)
+    }
+  }
+  
+  td <- spl_cause[[cause]] %>% purrr::reduce(full_join, by = "dose") %>% as.data.frame()
+  td <- reshape2::melt(td, id.vars="dose")
+
+  ggplot(td) +
+    aes(x = dose, y = value, linetype = variable) + geom_line() +
+    labs(x = "dose", y = "RR", title = cause) +
+    theme(legend.position = "none")
+
+  ggplot2::ggsave(paste0("./test/ithimr-", cause, ".png"))
+  
+}
+dev.off()
+
