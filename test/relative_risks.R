4edfc57e67033e4bae96771b231a09cd0aecc595
Ali Abbas <m.aliabbas@gmail.com>
2021-07-30 16:33:05 +0100

    Add comments

--- test/relative_risks.R
+++ test/relative_risks.R
@@ -182,11 +182,14 @@ rr_2
 rm(list = ls())
 require(ithimr)
 
+# Find global folder in ithimr package
 global_path <- file.path(find.package('ithimr',lib.loc=.libPaths()), 'extdata/global/')
 global_path <- paste0(global_path, "/")
 
+# Read disease lookup
 DISEASE_INVENTORY <- read.csv(paste0(global_path,"dose_response/disease_outcomes_lookup.csv"))
 
+# Read all DR PA curves
 list_of_files <- list.files(path = paste0(global_path,"dose_response/drpa/extdata/"), recursive = TRUE, pattern = "\\.csv$", full.names = TRUE)
 for (i in 1:length(list_of_files)){
   assign(stringr::str_sub(basename(list_of_files[[i]]), end = -5),
@@ -194,58 +197,77 @@ for (i in 1:length(list_of_files)){
          pos = 1)
 }
 
+# Set colours for all PA disease
 cols <- rainbow(sum(DISEASE_INVENTORY$physical_activity == 1))
+# Filter all causes for PA
 cause_indices <- which(DISEASE_INVENTORY$physical_activity == 1)
 
 ############# sample dose-response curves
+# Set sample size
 nsamples <- 100
+# Initialize PA DR quantile vars using samples
 parameters <- ithimr::ithim_setup_parameters(NSAMPLES=nsamples,
                                              PA_DOSE_RESPONSE_QUANTILE=T)
 
+# Shorten range of endometrial and breast cancer to 0.3-0.7
 parameters$PA_DOSE_RESPONSE_QUANTILE_endometrial_cancer <- runif(nsamples, 0.3, 0.7)
 parameters$PA_DOSE_RESPONSE_QUANTILE_breast_cancer <- runif(nsamples, 0.3, 0.7)
 
+# Create a list to store DR relationship for each cause and quantile
 spl_cause <- list()
+
+# Save plots in a file
 pdf('test/PA_dose_response_sample.pdf',width=11,height=11)
+# Set dimensions of plot window
 par(mar=c(5,5,2,1),mfrow=c(3,3))
+# Loop through all PA causes
 for ( j in 1:length(cause_indices)){
   cause <- as.character(DISEASE_INVENTORY$pa_acronym[cause_indices[j]])
   
+  # Select mortality or all (mortality and incidence combined) for all cause
   if (cause %in% c("all_cause", "coronary_heart_disease", "stroke", "diabetes", "total_cancer"))
     doses_vector <- get(paste0(cause, '_mortality')) %>% dplyr::select(dose) %>% unlist()
   else
     doses_vector <- get(paste0(cause, '_all')) %>% dplyr::select(dose) %>% unlist()
   
-  # get name of disease
-  # print(cause)
+  # Loop through samples
   for(seed in 1:nsamples){
+    # For all parameters quantiles, create a variable
     for(i in 1:length(parameters))
       assign(names(parameters)[i],parameters[[i]][[seed]],pos=1)
+    # Get PA DR relationship - which returns values according to the current quantile
     return_vector <- ithimr::PA_dose_response(cause = cause,dose = doses_vector)
     
-    # print(get(paste0('PA_DOSE_RESPONSE_QUANTILE_',cause)))
+    # Create a df for RR and dose
     temp_df <- data.frame(dose = doses_vector, RR = return_vector)
+    # Rename RR by adding sample number to it
     names(temp_df)[2] <- paste0("RR_", seed)
+    # Save RR and DF relationship in a list
     spl_cause[[cause]][[seed]] <- temp_df
     
+    # Plot dose and RR relationship
     if(seed==1){
-      plot(doses_vector,return_vector$rr,type='l',ylim=0:1,main=cause,ylab='Relative risk',xlab='Dose',
-           frame=F,col=cols[j],xlim=range(doses_vector),cex.axis=1.5,cex.lab=1.5,cex.main=1.5,lwd=1)
+      plot(doses_vector, return_vector$rr, type='l', ylim = 0:1, main = cause, 
+           ylab ='Relative risk', xlab = 'Dose', frame = F, col = cols[j], 
+           xlim = range(doses_vector), cex.axis = 1.5, cex.lab = 1.5, cex.main = 1.5, lwd = 1)
     }else{
-      lines(doses_vector,return_vector$rr,col=cols[j],lwd=1)
+      lines(doses_vector, return_vector$rr, col = cols[j], lwd = 1)
     }
   }
   
+  # Reduce list to a single df
   td <- spl_cause[[cause]] %>% purrr::reduce(full_join, by = "dose") %>% as.data.frame()
+  # Change wide to long format
   td <- reshape2::melt(td, id.vars="dose")
-
+  
+  # Plot individual lines - similar to plot above
   ggplot(td) +
     aes(x = dose, y = value, linetype = variable) + geom_line() +
     labs(x = "dose", y = "RR", title = cause) +
     theme(legend.position = "none")
-
+  # Save cause specific plot in a file
   ggplot2::ggsave(paste0("./test/ithimr-", cause, ".png"))
   
 }
+# Save pdf in a file
 dev.off()
-
